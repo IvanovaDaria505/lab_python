9315623d72d7155a65985f7322d707a0933d706d
IvanovaDaria505 <m2505667@edu.misis.ru>
2025-10-21 09:44:23 +0300

    123456789

--- src/text_report.py
+++ src/text_report.py
@@ -1,7 +1,6 @@
 from io_txt_csv import read_text, write_csv, ensure_parent_dir
 import sys
 from pathlib import Path
-import os
 
 sys.path.append(r'C:\Users\Home\Documents\GitHub\lab_01\lib')
 
@@ -9,21 +8,28 @@ from text import normalize, tokenize, count_freq, top_n
 
 
 def exist_path(path_f: str):
-    return os.path.exists(path_f)
+    return Path(path_f).exists()
 
 
 def main(file: str, encoding: str = 'utf-8'):
     if not exist_path(file):
-        return FileExistsError
+        return FileNotFoundError
+    
+    file_path = Path(file)
     text = read_text(file, encoding=encoding)
     norm = normalize(text)
     tokens = tokenize(norm)
-    top = top_n(count_freq(tokens), 5)
+    freq_dict = count_freq(tokens)
+    top = top_n(freq_dict, 5)
     top_sort = sorted(top, key=lambda x: (x[1], x[0]), reverse=True)
-    write_csv(top_sort, os.path.dirname(file) + r'\report.csv' , header=('word','count'))
+    report_path = file_path.parent / 'report.csv'
+    write_csv(top_sort, report_path, header=('word', 'count'))
+    
     print(f'Всего слов: {len(tokens)}')
-    print(f'Уникальных слов: {len(count_freq(tokens))}')
+    print(f'Уникальных слов: {len(freq_dict)}')
     print('Топ-5:')
     for cursor in top_sort:
         print(f'{cursor[0]}: {cursor[-1]}')
+
+
 main(r'C:\Users\Home\Documents\GitHub\lab_01\data\input.txt')
