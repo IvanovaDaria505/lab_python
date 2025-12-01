998df9d2133a4ed897d9e1f0b192887a39cc5f89
IvanovaDaria505 <m2505667@edu.misis.ru>
2025-12-01 21:13:58 +0300

    12344566

--- src/models.py
+++ src/models.py
@@ -1,6 +1,3 @@
-## Лабораторная работа 8
-### models.py
-```python
 from dataclasses import dataclass
 from datetime import datetime, date
 import re
@@ -68,48 +65,3 @@ if __name__ == "__main__":
         print(f"Словарь: {student.to_dict()}")
     except ValueError as e:
         print(f"Ошибка: {e}")
-```
-![alt text](<прога 8 1.png>)
-### serialize.py
-```python
-import json
-from typing import List
-from models import Student
-def students_to_json(students: List[Student], path: str) -> None:
-    data = [student.to_dict() for student in students]
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
-    print(f"Данные сохранены в {path}")
-def students_from_json(path: str) -> List[Student]:
-    try:
-        with open(path, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-        students = []
-        for item in data:
-            try:
-                student = Student.from_dict(item)
-                students.append(student)
-            except ValueError as e:
-                print(f"Не удалось создать студента из записи {item}: {e}")
-        print(f"Загружено {len(students)} студентов из {path}")
-        return students   
-    except FileNotFoundError:
-        print(f"Файл {path} не найден")
-        return []
-    except json.JSONDecodeError as e:
-        print(f"Ошибка при чтении JSON файла: {e}")
-        return []
-if __name__ == "__main__": 
-    students = [
-        Student("Иванов Иван Иванович", "2007-02-19", "БИВТ-1-1", 3.5),
-        Student("Петров Петр Петрович", "2006-01-01", "БИ-2-1", 4.5),
-        Student("Семёнов Семён Семёнович", "2005-03-03", "ПМ-3-1", 3.9)
-    ]
-    students_to_json(students, "data/students_output.json")
-    loaded_students = students_from_json("data/students_input.json")
-    for student in loaded_students:
-        print(student)
-```
-![alt text](<прога 8 2.png>)
-![alt text](<прога 8 3.png>)
-![alt text](<прога 8 4.png>)
